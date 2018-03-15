3ef8ffc6ba1e0c101881e0e1a244cba0281bd397
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2018-03-15 15:08:31 +0700

    first commit

---
+++ app/src/main/java/toong/vn/androidkotlinhelloworld/FindViewByIdActivity.kt
@@ -0,0 +1,20 @@
+package toong.vn.androidkotlinhelloworld
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+class FindViewByIdActivity : AppCompatActivity() {
+
+  lateinit var textView2: TextView
+  var textView3: TextView? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_find_view_by_id)
+
+    val textView = findViewById<TextView>(R.id.text_1)
+    textView2 = findViewById<TextView>(R.id.text_2)
+    textView3 = findViewById<TextView>(R.id.text_3)
+  }
+}
